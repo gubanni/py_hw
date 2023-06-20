71e679b23da6803f3b7dd1cea8d9dd4aa671ab04
gubanni <106946548+gubanni@users.noreply.github.com>
2023-06-20 19:21:51 +0300

    hw_6

---
+++ hw6_1.py
@@ -0,0 +1,23 @@
+#В модуль с проверкой даты добавьте возможность запуска в терминале с передачей даты на проверку.
+from datetime import datetime
+from sys import argv
+
+
+def date_validate(date_text: str) -> str:
+    try:
+        value = datetime.strptime(date_text, "%d.%m.%Y").date()
+        return "OK"
+    except:
+        return "Not OK"
+
+
+def _leap_info(date_text: str) -> bool:
+    year = int(date_text.split(".")[-1])
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    print(date_validate(argv[2]))
